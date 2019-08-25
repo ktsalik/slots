1ae19409ee3ce63c685d5675cc56fbf36c77b333
Kostas Tsalikidis <tsalikidhs@gmail.com>
2019-08-25 10:47:52 +0300

    ramses trasure resources

---
+++ data/games/ramses treasure/game.js
@@ -0,0 +1,172 @@
+var game = new Game(window.gameKey, 1500, 640);
+document.body.appendChild(game.view);
+
+window.addEventListener('resize', function () {
+  game.resize();
+});
+
+game.start({
+  resources: [
+    ['background', 'bg_game.jpg'],
+    ['menu-background', 'bg_menu.jpg'],
+    ['btn-menu-play', 'but_play_bg.png'],
+    ['mask', 'mask_slot.png'],
+    ['symbol-1', 'symbol-1.png'],
+    ['symbol-2', 'symbol-2.png'],
+    ['symbol-3', 'symbol-3.png'],
+    ['symbol-4', 'symbol-4.png'],
+    ['symbol-5', 'symbol-5.png'],
+    ['symbol-6', 'symbol-6.png'],
+    ['symbol-7', 'symbol-7.png'],
+    ['symbol-8', 'symbol-8.png'],
+    ['symbol-9', 'symbol-9.png'],
+    ['btn-spin', 'but_spin_bg.png'],
+    ['btn-exit', 'but_exit.png'],
+    ['btn-autoplay', 'but_maxbet_bg.png'],
+  ],
+}).then(function () {
+  var background = game.sprite('background');
+  var menuBackground = game.sprite('menu-background');
+  var btnMenuPlay = game.sprite('btn-menu-play');
+  btnMenuPlay._x = 600;
+  btnMenuPlay._y = 470;
+  btnMenuPlay.interactive = true;
+  var btnMenuPlayText = new PIXI.Text('PLAY', {
+    fontFamily: 'walibi',
+    fontSize: 30,
+    fill: '0xFFFFFF',
+  });
+  game.app.stage.addChild(btnMenuPlayText);
+  btnMenuPlayText._x = 705;
+  btnMenuPlayText._y = 535;
+  btnMenuPlayText.fontSize = 30;
+
+  var mask = game.sprite('mask');
+  mask.visible = false;
+
+  btnMenuPlay.on('click', function () {
+    menuBackground.visible = false;
+    btnMenuPlay.visible = false;
+    btnMenuPlayText.visible = false;
+
+    mask.visible = true;
+    reelsController.reels.forEach(function(reel) {
+      reel.setVisible(true);
+    });
+    btnSpin.visible = true;
+    creditsText.visible = true;
+    btnExit.visible = true;
+    btnAutoplay.visible = autoplayText.visible = true;
+  });
+
+  var reelsController = new ReelsController({
+    reels: [
+      {
+        positions: 3,
+        x: 380,
+        y: 117,
+      },
+      {
+        positions: 3,
+        x: 530,
+        y: 117,
+      },
+      {
+        positions: 3,
+        x: 680,
+        y: 117,
+      },
+      {
+        positions: 3,
+        x: 830,
+        y: 117,
+      },
+      {
+        positions: 3,
+        x: 980,
+        y: 117,
+      },
+    ],
+    symbolCount: 9,
+  }, game);
+  reelsController.reels.forEach(function(reel) {
+    reel.setVisible(false);
+  });
+
+  reelsController.onSpin = function(reelsState) {
+    if (reelsState == 'stopped') {
+      credits--;
+      creditsText.text = 'CREDITS: ' + credits;
+    }
+    if (autoplay) {
+      setTimeout(function() {
+        reelsController.spin();
+      }, 580 + 1000);
+    }
+  };
+
+  var btnSpin = game.sprite('btn-spin');
+  btnSpin.visible = false;
+  btnSpin._x = 995;
+  btnSpin._y = 510;
+  btnSpin.interactive = true;
+  btnSpin.on('click', function() {
+    window.autoplay = false;
+    reelsController.spin();
+  });
+
+  window.autoplay = false;
+  var btnAutoplay = game.sprite('btn-autoplay');
+  btnAutoplay.visible = false;
+  btnAutoplay._x = 750;
+  btnAutoplay._y = 570;
+  btnAutoplay.interactive = true;
+  btnAutoplay.on('click', function() {
+    window.autoplay = !window.autoplay;
+    if (window.autoplay)
+    reelsController.spin();
+  });
+
+  var autoplayText = new PIXI.Text('AUTOPLAY', {
+    fontFamily: 'walibi',
+    fontSize: 22,
+    fill: '0xFFFFFF',
+  });
+  autoplayText.visible = false;
+  game.app.stage.addChild(autoplayText);
+  autoplayText._x = 794;
+  autoplayText._y = 579;
+  autoplayText.fontSize = 22;
+
+  var credits = 111;
+  var creditsText = new PIXI.Text('CREDITS: ' + credits, {
+    fontFamily: 'walibi',
+    fontSize: 20,
+    fill: '0xFFFFFF',
+  });
+  creditsText.visible = false;
+  game.app.stage.addChild(creditsText);
+  creditsText._x = 380;
+  creditsText._y = 38;
+  creditsText.fontSize = 20;
+
+  var btnExit = game.sprite('btn-exit');
+  btnExit.visible = false;
+  btnExit._x = 1500 - 82;
+  btnExit.interactive = true;
+  btnExit.on('click', function() {
+    location.href = '../client';
+  });
+
+  game.resize();
+  game.resize();
+  reelsController.reels.forEach(function(reel) {
+    reel.resize();
+  });
+
+  window.addEventListener('resize', function() {
+    reelsController.reels.forEach(function(reel) {
+      reel.resize();
+    });
+  });
+});
